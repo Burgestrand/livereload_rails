947fb937008d81946bebafbe2225529b39d49954
Kim Burgestrand <kim@elabs.se>
2015-03-27 11:33:30 +0100

    Refactor stream to separate IO errors and write handling.

--- lib/livereload-rails/stream.rb
+++ lib/livereload-rails/stream.rb
@@ -6,6 +6,7 @@ module Livereload
   class Stream
     READ_CHUNK = 1024 * 10
     EMPTY = "".freeze
+    SWALLOW_ERRORS = [EOFError, IOError, Errno::EPIPE, Errno::ECONNRESET, Errno::EPROTOTYPE]
 
     # @example
     #   stream = Stream.new(io) do |input|
@@ -19,20 +20,17 @@ module Livereload
     # @yieldparam input [String] streaming input data.
     def initialize(io)
       @io = io
+      @io_writer = @io.dup
 
       @read_block = Proc.new
-      @stream_handler = method(:stream_handler)
-      @wakeup_handler = method(:wakeup_handler)
 
       @input_buffer = "".b
       @output_buffer = "".b
-
       @output_queue = []
       @mutex = Mutex.new
+
       @wakeup, @waker = IO.pipe
     end
-
-    attr_reader :io
 
     # Queue a message to be sent later on the stream.
     #
@@ -52,31 +50,42 @@ module Livereload
     # TODO: SO_LINGER, close before or after sending outgoing data?
     def close
       @io.close unless @io.closed?
+      @io_writer.close unless @io_writer.closed?
+      @waker.close unless @waker.closed?
+      @wakeup.close unless @wakeup.closed?
     end
 
-    # Continously stream data to/from the underlying IO.
+    # @return [Boolean] true if stream is closed.
+    def closed?
+      @io.closed?
+    end
+
+    # @param [NIO::Selector] selector
     def loop(selector = NIO::Selector.new)
-      return if @io.closed?
+      @looping = ! closed?
+      return unless @looping
 
-      @selector = selector
+      wakeup_monitor = selector.register(@wakeup, :r)
+      wakeup_monitor.value = handler_for(:wakeup_handler)
 
-      selector.register(@wakeup, :r).value = @wakeup_handler
-      register_stream(:r)
+      read_monitor = selector.register(@io, :r)
+      read_monitor.value = handler_for(:read_handler)
 
-      while selector.registered?(@io)
-        # TODO: timeouts, see https://github.com/celluloid/nio4r/issues/63
-        @selector.select do |monitor|
-          monitor.value.call(monitor)
-        end
+      register_writer(selector)
+
+      while @looping
+        selector.select { |monitor| monitor.value.call(monitor) }
       end
     ensure
-      @selector = nil
       selector.deregister(@io)
+      selector.deregister(@io_writer)
       selector.deregister(@wakeup)
     end
 
     private
 
+    # @note The returned string is always the same object.
+    # @return [String] the current output buffer, possibly refilled from the output queue.
     def output_buffer
       if @output_buffer.empty? and @output_queue.length > 0
         message = @mutex.synchronize { @output_queue.pop }
@@ -86,65 +95,51 @@ module Livereload
       @output_buffer
     end
 
-    def register_stream(interests)
-      if @stream_monitor
-        if @stream_monitor.interests == interests
-          return @stream_monitor
-        else
-          @stream_monitor.close
+    def register_writer(selector)
+      return if selector.registered?(@io_writer)
+      return if output_buffer.empty?
+
+      write_monitor = selector.register(@io_writer, :w)
+      write_monitor.value = handler_for(:write_handler)
+    end
+
+    def handler_for(method_name)
+      handler_method = method(method_name)
+
+      lambda do |monitor|
+        begin
+          handler_method.call(monitor)
+        rescue IO::WaitReadable, IO::WaitWritable
+          # No op. Let monitor continue be selected.
+        rescue *SWALLOW_ERRORS
+          @looping = false
+        ensure
+          @looping = false if $!
         end
       end
-
-      @stream_monitor = @selector.register(@io, interests)
-      @stream_monitor.value = @stream_handler
-      @stream_monitor
     end
 
     def wakeup_handler(monitor)
-      monitor.io.read(monitor.io.stat.size)
-      register_stream(:rw)
+      @wakeup.read(@wakeup.stat.size)
+      register_writer(monitor.selector)
     end
 
-    # This method takes care of reading and writing as much data to/from the IO
-    # in a single pass.
-    def stream_handler(monitor)
-      next_interests = :r
-
-      if monitor.readable?
-        begin
-          # Read as much data as possible.
-          until @io.closed?
-            @io.read_nonblock(READ_CHUNK, @input_buffer)
-            @read_block[@input_buffer]
-          end
-        rescue IO::WaitReadable
-          # No op. Next interest is always at least a read.
-        ensure
-          @input_buffer.clear
-        end
+    def write_handler(monitor)
+      until output_buffer.empty?
+        bytes_written = @io.write_nonblock(output_buffer)
+        output_buffer[0, bytes_written] = EMPTY
       end
 
-      begin
-        # Write as much data as possible.
-        while not @io.closed? and output_buffer.bytesize > 0
-          bytes_written = @io.write_nonblock(output_buffer)
-          output_buffer[0, bytes_written] = EMPTY
-        end
-      rescue IO::WaitWritable
-        next_interests = :rw
-      end
+      monitor.close # write_nonblock did not raise, so we have no more output.
+    end
 
-      if @io.closed?
-        monitor.close
-      else
-        register_stream(next_interests)
+    def read_handler(monitor)
+      Kernel.loop do
+        @io.read_nonblock(READ_CHUNK, @input_buffer)
+        @read_block[@input_buffer]
       end
-    rescue EOFError, IOError, Errno::EPIPE, Errno::ECONNRESET, Errno::EPROTOTYPE
-      # Swallow errors.
-      monitor.close
     ensure
-      # Other errors bubble.
-      monitor.close if $!
+      @input_buffer.clear
     end
   end
 end
