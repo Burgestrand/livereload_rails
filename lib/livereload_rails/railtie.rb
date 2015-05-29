a8c9728755c79e2fe604b7c2a13369f3722071c0
Kim Burgestrand <kim@elabs.se>
2015-05-29 15:50:26 +0200

    Fix bug with rake tasks.

--- lib/livereload_rails/railtie.rb
+++ lib/livereload_rails/railtie.rb
@@ -1,10 +1,11 @@
 require "livereload_rails"
 require "rails/engine"
+require "rails/commands/server"
 require "rack-livereload"
 
 module LivereloadRails
   class Railtie < ::Rails::Engine
-    config.app_middleware.use Rack::LiveReload, live_reload_port: Rails::Server.new.options[:Port]
+    config.app_middleware.use Rack::LiveReload, live_reload_port: ::Rails::Server.new.options[:Port]
     config.app_middleware.use LivereloadRails::Middleware, assets: config.assets
   end
 end
